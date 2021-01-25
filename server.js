33006a136627c9ee2376acf84ec144032e7081e0
johnny <>
2021-01-25 17:57:16 +0800

    update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// 读取当前目录下环境变量port的值,如果沒設定,就設定5000
 const PORT = process.env.PORT || 5000;
-
+// 監聽這個port
 app.listen(PORT, () => console.log(`Service started on port ${PORT}`));
