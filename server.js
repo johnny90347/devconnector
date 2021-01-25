4fd5e44861b3dcde9993393253bdf22c491fe6ee
johnny <>
2021-01-25 17:05:44 +0800

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+// 載入模組
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Service started on port ${PORT}`));
